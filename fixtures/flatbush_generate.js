22d11ee0530b06c749c6efe8180cd0e0b8725792
Kyle Barron <kylebarron2@gmail.com>
2023-11-05 00:03:23 -0400

    Use intersecting locate variant for rstar search

--- fixtures/flatbush_generate.js
+++ fixtures/flatbush_generate.js
@@ -4,20 +4,28 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(__dirname);
 
-const buffer = readFileSync(`${__dirname}/data1_input.raw`);
-const data = new Float64Array(
-  buffer.buffer,
-  buffer.byteOffset,
-  buffer.byteLength / Float64Array.BYTES_PER_ELEMENT
-);
+function generateFlatbushBuffer(infile, outfile) {
+  const buffer = readFileSync(`${__dirname}/${infile}`);
+  const data = new Float64Array(
+    buffer.buffer,
+    buffer.byteOffset,
+    buffer.byteLength / Float64Array.BYTES_PER_ELEMENT
+  );
 
-const index = new Flatbush(data.length / 4);
-for (let i = 0; i < data.length; i += 4) {
-  index.add(data[i], data[i + 1], data[i + 2], data[i + 3]);
+  const index = new Flatbush(data.length / 4);
+  for (let i = 0; i < data.length; i += 4) {
+    index.add(data[i], data[i + 1], data[i + 2], data[i + 3]);
+  }
+
+  index.finish();
+
+  writeFileSync(`${__dirname}/${outfile}`, new Uint8Array(index.data));
 }
 
-index.finish();
+function main() {
+  generateFlatbushBuffer("data1_input.raw", "data1_flatbush_js.raw");
+  generateFlatbushBuffer("utah_input.raw", "utah_flatbush_js.raw");
+}
 
-writeFileSync(`${__dirname}/data1_flatbush_js.raw`, new Uint8Array(index.data));
+main()
