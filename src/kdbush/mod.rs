bcc7eb4ab68fa3140098f9c84cca0620e946e7db
Kyle Barron <kylebarron2@gmail.com>
2023-11-07 00:31:50 -0500

    Add kdbush searching

--- src/kdbush/mod.rs
+++ src/kdbush/mod.rs
@@ -2,6 +2,7 @@ pub mod builder;
 pub mod constants;
 pub mod error;
 pub mod index;
+pub mod r#trait;
 
 pub use builder::KdbushBuilder;
-pub use index::OwnedKdbush;
+pub use index::{KdbushRef, OwnedKdbush};
