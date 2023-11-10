8195b44384b28aa57db14cd969404db3287d50ac
Kyle Barron <kylebarron2@gmail.com>
2023-11-10 00:10:26 -0500

    Add kdbush tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod kdbush;
 pub mod r#type;
 
 pub use flatbush::{FlatbushBuilder, FlatbushIndex, FlatbushRef, OwnedFlatbush};
-pub use kdbush::{KdbushBuilder, OwnedKdbush};
+pub use kdbush::{KdbushBuilder, KdbushIndex, KdbushRef, OwnedKdbush};
 
 #[cfg(test)]
 pub(crate) mod test;
