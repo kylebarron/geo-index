bb1bf4beb4c4f641b472ca162e189fed0002a9fc
Kyle Barron <kylebarron2@gmail.com>
2024-01-14 21:53:29 -0500

    Make IndexableNum sealed (#24)

--- src/type.rs
+++ src/type.rs
@@ -2,8 +2,23 @@ use std::fmt::Debug;
 
 use num_traits::{Bounded, Num, NumCast, ToPrimitive};
 
+/// A trait for types that can be used for indexed coordinates.
+///
+/// This trait is sealed and cannot be implemented for external types. This is because we want to
+/// ensure FFI compatibility with other implementations, including the reference implementations in
+/// JavaScript ([rtree](https://github.com/mourner/flatbush),
+/// [kdtree](https://github.com/mourner/kdbush))
 pub trait IndexableNum:
-    Num + NumCast + ToPrimitive + PartialOrd + Debug + Send + Sync + bytemuck::Pod + Bounded
+    private::Sealed
+    + Num
+    + NumCast
+    + ToPrimitive
+    + PartialOrd
+    + Debug
+    + Send
+    + Sync
+    + bytemuck::Pod
+    + Bounded
 {
     /// The type index to match the array order of `ARRAY_TYPES` in flatbush JS
     const TYPE_INDEX: u8;
@@ -49,4 +64,17 @@ impl IndexableNum for f32 {
 impl IndexableNum for f64 {
     const TYPE_INDEX: u8 = 8;
     const BYTES_PER_ELEMENT: usize = 8;
+}
+// https://rust-lang.github.io/api-guidelines/future-proofing.html#sealed-traits-protect-against-downstream-implementations-c-sealed
+mod private {
+    pub trait Sealed {}
+
+    impl Sealed for i8 {}
+    impl Sealed for u8 {}
+    impl Sealed for i16 {}
+    impl Sealed for u16 {}
+    impl Sealed for i32 {}
+    impl Sealed for u32 {}
+    impl Sealed for f32 {}
+    impl Sealed for f64 {}
 }
