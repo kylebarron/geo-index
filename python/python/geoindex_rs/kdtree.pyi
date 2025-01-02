124677041e482013f9d654b805abddc53c94765b
Kyle Barron <kylebarron2@gmail.com>
2025-01-02 18:48:31 -0500

    Add kdtree docs

--- python/python/geoindex_rs/kdtree.pyi
+++ python/python/geoindex_rs/kdtree.pyi
@@ -12,8 +12,16 @@ if sys.version_info > (3, 12):
 else:
     from typing_extensions import Buffer
 
-ArrayLike = Union[np.ndarray, ArrowArrayExportable, memoryview, bytes]
-IndexLike = Union[np.ndarray, ArrowArrayExportable, memoryview, bytes, KDTree]
+ArrayLike = Union[np.ndarray, ArrowArrayExportable, Buffer]
+"""A type alias for accepted input to the [`KDTreeBuilder.add`][geoindex_rs.KDtree.KDTreeBuilder.add] method.
+
+Accepted input includes numpy arrays, Arrow arrays, and C-contiguous buffer protocol
+input.
+"""
+
+IndexLike = Union[np.ndarray, ArrowArrayExportable, Buffer, KDTree]
+"""A type alias for accepted input as a KDTree.
+"""
 
 def range(
     index: IndexLike,
@@ -21,24 +29,123 @@ def range(
     min_y: int | float,
     max_x: int | float,
     max_y: int | float,
-) -> Array: ...
+) -> Array:
+    """Search a KDTree for elements intersecting the provided bounding box.
+
+    Results are the insertion indexes of items that match the query.
+
+    Args:
+        index: the KDTree to search
+        min_x: The `min_x` coordinate of the query bounding box
+        min_y: The `min_y` coordinate of the query bounding box
+        max_x: The `max_x` coordinate of the query bounding box
+        max_y: The `max_y` coordinate of the query bounding box
+
+    Returns:
+        An Arrow array with the insertion indexes of query results.
+    """
 def within(
     index: IndexLike,
     qx: int | float,
     qy: int | float,
     r: int | float,
-) -> Array: ...
+) -> Array:
+    """Search a KDTree for elements within the given distance of the query point.
+
+    Results are the insertion indexes of items that match the query.
+
+    Args:
+        index: the KDTree to search
+        qx: The `x` coordinate of the query point
+        qy: The `y` coordinate of the query point
+        r: The radius from the query point to use for searching.
+
+    Returns:
+        An Arrow array with the insertion indexes of query results.
+    """
 
 class KDTreeBuilder:
+    """A builder class to create a [KDTree][geoindex_rs.kdtree.KDTree].
+
+    **Example:**
+
+    ```py
+    import numpy as np
+    from geoindex_rs import kdtree as kd
+
+    builder = kd.KDTreeBuilder(3)
+    x = np.arange(0, 3)
+    y = np.arange(2, 5)
+    builder.add(x, y)
+    tree = builder.finish()
+    ```
+    """
     def __init__(
         self,
         num_items: int,
         node_size: int = 64,
-        coord_type: Literal["float32", "float64", None] = None,
-    ) -> None: ...
-    def add(self, x: ArrayLike, y: ArrayLike | None = None) -> Array: ...
-    def finish(self) -> KDTree: ...
+        coord_type: Literal["float32", "float64"] = "float64",
+    ) -> None:
+        """Initialize a KDTree with the given parameters.
+
+        This will provision memory for the KDTree, to be filled in the [`add`][geoindex_rs.kdtree.KDTreeBuilder.add] method.
+
+        Args:
+            num_items: The number of items in the tree
+            node_size: The node size of the tree. Defaults to 64.
+            coord_type: The coordinate type to use in the tree. Currently only float32
+                and float64 are permitted. Defaults to "float64".
+        """
+    def add(self, x: ArrayLike, y: ArrayLike | None = None) -> Array:
+        """Insert points in this KDTree.
+
+        There are multiple ways to call this method:
+
+        - Two numpy or Arrow numeric arrays, passed as `x`, `y`.
+        - A 2D numpy array, with shape `(N, 2)`. It must be C-contiguous.
+        - One argument with an Arrow array of type FixedSizeList or Struct. The
+          FixedSizeListArray must have a `list_size` of 2, and the `StructArray` must
+          have 2 children, ordered `x` and `y`.
+
+          In this case, all other parameters should be left as `None`.
+
+
+        !!! note
+            Most of the time, you should call `add` only once. This function is
+            vectorized, and you should avoid calling it in a loop with a few rows of
+            data at a time.
+
+            In some cases, it may be useful to call this in a loop, such as if the input
+            geometries do not all fit in memory at once.
+
+        It's important to add _arrays_ at a time. This should usually not be called in a loop.
+
+        Args:
+            x: array-like input
+            y: array-like input. Defaults to None.
+
+        Returns:
+            An Arrow array with the insertion index of each element, which provides a lookup back into the original data.
+        """
+    def finish(self) -> KDTree:
+        """Sort the internal index and convert this class to a KDTree instance.
+
+        Returns:
+            An immutable KDTree instance, which can be used for spatial queries.
+        """
     def __repr__(self) -> str: ...
 
 class KDTree(Buffer):
+    """A fast, memory-efficient, zero-copy-compatible, 2D KDTree.
+
+    Use [`KDTreeBuilder`][geoindex_rs.kdtree.KDTreeBuilder], and then call
+    [`finish`][geoindex_rs.kdtree.KDTreeBuilder.finish] to construct this.
+
+    This uses a binary-stable memory layout under the hood. So you can write the index
+    to disk, then later load it and perform queries on it.
+
+    This class implements the Python buffer protocol, so you can pass it to the Python
+    `bytes` constructor to copy the underlying binary memory into a Python `bytes`
+    object.
+    """
     def __repr__(self) -> str: ...
